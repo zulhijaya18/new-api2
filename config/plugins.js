b7a4bff2f28f775c889e6627e4203db9c49a1c2a
zulhijaya18 <32.zulhijaya@gmail.com>
2022-04-21 14:48:02 +0800

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  "website-builder": {
+    enabled: true,
+    config: {
+      url: "https://link-to-hit-on-trigger.com",
+      trigger: {
+        type: "manual",
+      },
+    },
+  },
+});
